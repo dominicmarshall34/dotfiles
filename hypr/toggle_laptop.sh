6cc86ffe4d7f0506433b9400c437fd6fe1a1288d
dom <dom@marshall34.co.uk>
2025-12-26 21:12:12 +0000

    Turn off tap to click for trackpad and fix monitor toggle

--- hypr/toggle_laptop.sh
+++ hypr/toggle_laptop.sh
@@ -1,10 +1,11 @@
 #!/bin/bash
 
-# Check if eDP-1 is disabled
-if hyprctl monitors all | grep -A 20 "eDP-1" | grep -q "disabled: false"; then
-    # If enabled, disable it
+# hyprctl monitors (without 'all') only lists ENABLED monitors.
+# So we just check if eDP-1 is in that list.
+if hyprctl monitors | grep -q "eDP-1"; then
+    # It's currently ON, so disable it
     hyprctl keyword monitor "eDP-1, disable"
 else
-    # If disabled, enable it with preferred settings
+    # It's currently OFF, so enable it
     hyprctl keyword monitor "eDP-1, 2256x1504@60, 0x0, 1.566667"
 fi
